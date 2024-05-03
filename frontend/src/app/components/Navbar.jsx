6f75a570273124d6a6e523bcd6bffd5c83d63a7e
shantihedelin <145372686+shantihedelin@users.noreply.github.com>
2024-05-03 12:57:58 +0200

    added startpage and made navbar component (commented out in layout.js for now)

---
+++ frontend/src/app/components/Navbar.jsx
@@ -0,0 +1,9 @@
+export default function Navbar() {
+
+    return (
+        <main className="text-white bg-gray-800 flex justify-between py-4">
+            <div>Logo</div>
+            <div>Hamburgermenu</div>
+        </main>
+    )
+}
