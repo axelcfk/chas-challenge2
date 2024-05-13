b6cfc73e6feed4905e6d0d261ad0385b74f363d9
shantihedelin <145372686+shantihedelin@users.noreply.github.com>
2024-05-13 12:42:02 +0200

    design and function on profile page

--- frontend/src/app/profile/[userId]/page.jsx
+++ frontend/src/app/profile/[userId]/page.jsx
@@ -1,5 +1,6 @@
 "use client";
 
+import "./profile.css";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import Navbar from "@/app/components/Navbar";
@@ -7,21 +8,24 @@ import SlideMenu, { SlideMenuMovieCard } from "@/app/components/SlideMenu";
 
 export default function Profile() {
   const [userData, setUserData] = useState(null);
+  //standard menyn som visas när man besöker sidan är "profile"
+  const [activeTab, setActiveTab] = useState("Profile");
   const router = useRouter();
+
+  const tabNames = ["Profile", "Watchlist", "My lists"];
+  const tabIndex = tabNames.indexOf(activeTab);
 
   useEffect(() => {
     const token = localStorage.getItem("token");
-
     if (!token) {
       router.push("/login");
       return;
     }
     const userId = localStorage.getItem("userId");
-    // Logga userId
+
     console.log("Current userId:", userId);
     console.log("Current userData:", userData);
 
-    // Exempel på API-anrop för att hämta användardata
     fetch(`http://localhost:3010/users/${userId}`, {
       headers: {
         Authorization: `Bearer ${token}`,
@@ -39,7 +43,13 @@ export default function Profile() {
 
   console.log("second userdata: ", userData);
 
-  //* Lägg bakgrundsfärgen globalt senare
+  const handleTabClick = (tab) => {
+    setActiveTab(tab);
+  };
+
+
+  const xOffset = tabIndex * -100;
+
   //* Navbaren ska importeras globalt i layout senare
 
   return (
@@ -51,44 +61,63 @@ export default function Profile() {
             <button className="bg-transparent border-none hover:cursor-pointer">
               <img src="/profile-user.svg" className="h-28"></img>
             </button>
-
             <h1>{userData.username}</h1>
           </div>
         ) : (
           <p>Loading user data...</p>
         )}
       </div>
-      <div className="menu bg-[#110A19] flex flex-row justify-between px-8 items-center mt-12 h-16">
-        <h4>Profile</h4>
-        <h4>Watchlist</h4>
-        <h4>My lists</h4>
+      <div className="menu bg-[#110A19] flex flex-row justify-between px-8 items-center mt-12 h-16 mb-8">
+        {["Profile", "Watchlist", "My lists"].map((tab) => (
+          <button
+            key={tab}
+            onClick={() => handleTabClick(tab)}
+            className={`text-white border-none hover:cursor-pointer h-16 w-28 ${activeTab === tab ? 'active-tab' : 'bg-transparent'
+          }`} 
+          >
+            <h4>{tab}</h4>
+          </button>
+        ))}
       </div>
-      <div className="bg-[#201430] p-8">
-        <div className="mb-8">
-          <h3 className="text-2xl">My favorites, only 3st</h3>
-          <p>
-            Här ska man kunna trycka på en knapp så man får upp alla filmer som
-            ligger i sin "seen lista" och välja 3st favoriter som visas nedan.
-          </p>
-          <SlideMenu>
-            <SlideMenuMovieCard poster={"/troll-poster.jpg"} />
-            <SlideMenuMovieCard poster={"/troll-poster.jpg"} />
-            <SlideMenuMovieCard poster={"/troll-poster.jpg"} />
-          </SlideMenu>
+      {activeTab === "Profile" && (
+        <div className="gradient-border-top bg-[#201430] p-8 ">
+          <div className="my-8">
+            <h3 className="text-2xl">My favorites</h3>
+            <p className="text-sm">
+              Här ska man kunna trycka på en knapp så man får upp alla filmer
+              som ligger i sin "seen lista" och välja 3st favoriter som visas
+              nedan.
+            </p>
+            <SlideMenu>
+              <SlideMenuMovieCard poster={"/troll-poster.jpg"} />
+              <SlideMenuMovieCard poster={"/troll-poster.jpg"} />
+              <SlideMenuMovieCard poster={"/troll-poster.jpg"} />
+            </SlideMenu>
+          </div>
+          <div className="mb-8">
+            <h3 className="text-2xl">Recent activity</h3>
+            <p className="text-sm">
+              Här visas dom senaste filmerna man har kollat på (=lagt till i sin
+              seen/watchlist)
+            </p>
+            <SlideMenu>
+              <SlideMenuMovieCard poster={"/troll-poster.jpg"} />
+              <SlideMenuMovieCard poster={"/troll-poster.jpg"} />
+              <SlideMenuMovieCard poster={"/troll-poster.jpg"} />
+            </SlideMenu>
+          </div>
         </div>
-        <div className="mb-8">
-          <h3 className="text-2xl">Recent activity</h3>
-          <p>
-            Här visas dom senaste filmerna man har kollat på (=lagt till i sin
-            seen/watchlist)
-          </p>
-          <SlideMenu>
-            <SlideMenuMovieCard poster={"/troll-poster.jpg"} />
-            <SlideMenuMovieCard poster={"/troll-poster.jpg"} />
-            <SlideMenuMovieCard poster={"/troll-poster.jpg"} />
-          </SlideMenu>
+      )}
+      {activeTab === "Watchlist" && (
+        <div className="gradient-border-top bg-[#201430] p-8 ">
+          <p className="py-8">Content for watchlist</p>
         </div>
-      </div>
+      )}
+      {activeTab === "My lists" && (
+        <div className="gradient-border-top bg-[#201430] p-8 ">
+          <p className="py-8">Content for my lists</p>
+        </div>
+      )}
     </main>
   );
 }
