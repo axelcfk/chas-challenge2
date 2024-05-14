e7c77c47c0b91b92fe21442740a758fc6ff0d50e
afzbehroz <145428759+afzbehroz@users.noreply.github.com>
2024-05-14 14:44:53 +0200

    About

--- frontend/src/app/components/Navbar.jsx
+++ frontend/src/app/components/Navbar.jsx
@@ -115,7 +115,7 @@ export default function Navbar() {
                 </Link>
               </>
             )}
-            <Link href="/services" style={{ textDecoration: "none" }}>
+            <Link href="/about" style={{ textDecoration: "none" }}>
               <span className="hover:bg-lighter-purple px-3 py-2 rounded-md text-base font-medium cursor-pointer block text-center text-white">
                 About
               </span>
