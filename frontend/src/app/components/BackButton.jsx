911cf3ce45ed6fc739f0c5bf59aa153f429f0fa1
axelcfk <131769961+axelcfk@users.noreply.github.com>
2024-05-15 15:34:06 +0200

    .

--- frontend/src/app/components/BackButton.jsx
+++ frontend/src/app/components/BackButton.jsx
@@ -2,14 +2,12 @@
 import { useRouter } from "next/navigation";
 
 const BackButton = () => {
-  function handleBack() {}
-
   const router = useRouter();
   return (
     <div>
       <button
         className="bg-slate-400 rounded-full text-black text-lg mt-5 px-4 py-3 cursor-pointer"
-        onClick={handleBack}
+        onClick={router.back}
       >
         🔙
       </button>
