862d28321551f25f23571b5232d3ee4678b5eacd
shantihedelin <145372686+shantihedelin@users.noreply.github.com>
2024-05-29 12:42:06 +0200

    fixed issue with navbar not showing

--- frontend/src/app/components/Navbar.jsx
+++ frontend/src/app/components/Navbar.jsx
@@ -49,163 +49,163 @@ export default function Navbar() {
     router.push("/");
   };
 
+  if (!isLoggedIn || ['/firstpage', '/login', '/create-account'].includes(pathname)) {
+    return null;
+  }
+
   return (
-    <>
-      {isLoggedIn && pathname !== '/startpage' && pathname !== '/login' && pathname !== '/create-account' && (
-        <nav className="bg-[#110A1A] text-white w-full absolute top-0 z-50">
-          <div className="mx-auto px-4 sm:px-6 lg:px-8">
-            <div className="relative flex items-center justify-between h-16 ">
-              {!isSearchOpen && (
-                <div className="flex items-center">
-                  <div className="flex-shrink-0">
-                    <Link className="no-underline" href="/startpage">
-                      <span className="font-bebas flex justify-center items-center hover:cursor-pointer px-2 py-2 mr-1 text-white rounded-md font-medium cursor-pointer text-3xl text-center">
-                        <span className="text-xl">
-                          <FaDotCircle />
-                        </span>
-                        <span className="font-archivo font-extrabold no-underline">
-                          BAMMS
-                        </span>
-                      </span>
-                    </Link>
-                  </div>
-                </div>
-              )}
-              <div className="w-full">
-                <InputField
-                  input={input}
-                  handleInputChange={(e) => setInput(e.target.value)}
-                  handleQuerySubmit={handleQuerySubmit}
-                  heightDiv={"h-10"}
-                  placeholder={"AI SEARCH"}
-                />
+    <nav className="bg-[#110A1A] text-white w-full absolute top-0 z-50">
+      <div className="mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="relative flex items-center justify-between h-16 ">
+          {!isSearchOpen && (
+            <div className="flex items-center">
+              <div className="flex-shrink-0">
+                <Link className="no-underline" href="/startpage">
+                  <span className="font-bebas flex justify-center items-center hover:cursor-pointer px-2 py-2 mr-1 text-white rounded-md font-medium cursor-pointer text-3xl text-center">
+                    <span className="text-xl">
+                      <FaDotCircle />
+                    </span>
+                    <span className="font-archivo font-extrabold no-underline">
+                      BAMMS
+                    </span>
+                  </span>
+                </Link>
               </div>
-              <div className="flex items-center md:hidden w-full justify-end">
-                {!isSearchOpen && (
-                  <button
-                    onClick={() => setIsOpen(!isOpen)}
-                    className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:cursor-pointer  focus:outline-none border-none   focus:text-white bg-transparent ml-4"
-                    aria-label="Burger Menu"
-                  >
-                    <svg
-                      className="h-6 w-6"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                    >
-                      {isOpen ? (
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth={2}
-                          d="M6 18L18 6M6 6l12 12"
-                        />
-                      ) : (
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth={2}
-                          d="M4 6h16M4 12h16M4 18h16"
-                        />
-                      )}
-                    </svg>
-                  </button>
-                )}
-              </div>
-              <div
-                className={`menu-modal ${
-                  isOpen ? "open" : "close"
-                } flex-col md:hidden z-10 absolute w-full left-0 right-0`}
+            </div>
+          )}
+          <div className="w-full">
+            <InputField
+              input={input}
+              handleInputChange={(e) => setInput(e.target.value)}
+              handleQuerySubmit={handleQuerySubmit}
+              heightDiv={"h-10"}
+              placeholder={"AI SEARCH"}
+            />
+          </div>
+          <div className="flex items-center md:hidden w-full justify-end">
+            {!isSearchOpen && (
+              <button
+                onClick={() => setIsOpen(!isOpen)}
+                className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:cursor-pointer  focus:outline-none border-none   focus:text-white bg-transparent ml-4"
+                aria-label="Burger Menu"
               >
-                <div className="mt-16">
-                  {isLoggedIn ? (
-                    <>
-                      <Link href="/about" style={{ textDecoration: "none" }}>
-                        <span className="px-3 py-8 text-4xl rounded-md font-extrabold font-archivo cursor-pointer block text-center text-white">
-                          About
-                        </span>
-                      </Link>
-                      {user && user.id && (
-                        <Link
-                          onClick={() => setIsOpen(false)}
-                          href={`/profile/${user.id}`}
-                          style={{ textDecoration: "none" }}
-                        >
-                          <span className="px-3 py-8 rounded-md font-extrabold font-archivo cursor-pointer block text-center text-white">
-                            Profile
-                          </span>
-                        </Link>
-                      )}
-                      <Link
-                        href="/"
-                        onClick={handleLogout}
-                        style={{ textDecoration: "none" }}
-                      >
-                        <span className="px-3 pt-24 rounded-md font-extrabold font-archivo cursor-pointer block text-center text-white">
-                          Log Out
-                        </span>
-                      </Link>
-                    </>
-                  ) : (
-                    <>
-                      <Link href="/about" style={{ textDecoration: "none" }}>
-                        <span className="px-3 py-2 rounded-md text-base font-medium cursor-pointer block text-center text-white">
-                          About
-                        </span>
-                      </Link>
-                      <Link href="/login" style={{ textDecoration: "none" }}>
-                        <span className="px-3 py-2 rounded-md text-base font-medium cursor-pointer block text-center text-white">
-                          Log in
-                        </span>
-                      </Link>
-                    </>
-                  )}
-                </div>
-              </div>
-              <div className="hidden md:flex items-center">
-                {isLoggedIn ? (
-                  <>
-                    <MovieSearch
-                      isSearchOpen={isSearchOpen}
-                      setIsSearchOpen={setIsSearchOpen}
+                <svg
+                  className="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  {isOpen ? (
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M6 18L18 6M6 6l12 12"
                     />
-                    <Link href="/about" style={{ textDecoration: "none" }}>
-                      <span className="hover:bg-lighter-purple px-3 py-2 rounded-md text-xl font-bold font-archivo cursor-pointer block text-center text-white">
-                        About
-                      </span>
-                    </Link>
-                    {user && user.id && (
-                      <Link
-                        href={`/profile/${user.id}`}
-                        style={{ textDecoration: "none" }}
-                      >
-                        <span className="hover:bg-lighter-purple px-3 py-2 rounded-md text-base font-medium cursor-pointer block text-center text-white">
-                          Profile
-                        </span>
-                      </Link>
-                    )}
+                  ) : (
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M4 6h16M4 12h16M4 18h16"
+                    />
+                  )}
+                </svg>
+              </button>
+            )}
+          </div>
+          <div
+            className={`menu-modal ${
+              isOpen ? "open" : "close"
+            } flex-col md:hidden z-10 absolute w-full left-0 right-0`}
+          >
+            <div className="mt-16">
+              {isLoggedIn ? (
+                <>
+                  <Link href="/about" style={{ textDecoration: "none" }}>
+                    <span className="px-3 py-8 text-4xl rounded-md font-extrabold font-archivo cursor-pointer block text-center text-white">
+                      About
+                    </span>
+                  </Link>
+                  {user && user.id && (
                     <Link
-                      href="/"
-                      onClick={handleLogout}
+                      onClick={() => setIsOpen(false)}
+                      href={`/profile/${user.id}`}
                       style={{ textDecoration: "none" }}
                     >
-                      <span className="hover:bg-lighter-purple px-3 py-2 rounded-md text-xl font-bold font-archivo cursor-pointer block text-center text-white">
-                        Log Out
+                      <span className="px-3 py-8 rounded-md font-extrabold font-archivo cursor-pointer block text-center text-white">
+                        Profile
                       </span>
                     </Link>
-                  </>
-                ) : (
+                  )}
+                  <Link
+                    href="/"
+                    onClick={handleLogout}
+                    style={{ textDecoration: "none" }}
+                  >
+                    <span className="px-3 pt-24 rounded-md font-extrabold font-archivo cursor-pointer block text-center text-white">
+                      Log Out
+                    </span>
+                  </Link>
+                </>
+              ) : (
+                <>
+                  <Link href="/about" style={{ textDecoration: "none" }}>
+                    <span className="px-3 py-2 rounded-md text-base font-medium cursor-pointer block text-center text-white">
+                      About
+                    </span>
+                  </Link>
                   <Link href="/login" style={{ textDecoration: "none" }}>
-                    <span className="hover:bg-lighter-purple px-3 py-2 rounded-md text-xl font-archivo cursor-pointer block text-center text-white">
+                    <span className="px-3 py-2 rounded-md text-base font-medium cursor-pointer block text-center text-white">
                       Log in
                     </span>
                   </Link>
-                )}
-              </div>
+                </>
+              )}
             </div>
           </div>
-        </nav>
-      )}
-    </>
+          <div className="hidden md:flex items-center">
+            {isLoggedIn ? (
+              <>
+                <MovieSearch
+                  isSearchOpen={isSearchOpen}
+                  setIsSearchOpen={setIsSearchOpen}
+                />
+                <Link href="/about" style={{ textDecoration: "none" }}>
+                  <span className="hover:bg-lighter-purple px-3 py-2 rounded-md text-xl font-bold font-archivo cursor-pointer block text-center text-white">
+                    About
+                  </span>
+                </Link>
+                {user && user.id && (
+                  <Link
+                    href={`/profile/${user.id}`}
+                    style={{ textDecoration: "none" }}
+                  >
+                    <span className="hover:bg-lighter-purple px-3 py-2 rounded-md text-base font-medium cursor-pointer block text-center text-white">
+                      Profile
+                    </span>
+                  </Link>
+                )}
+                <Link
+                  href="/"
+                  onClick={handleLogout}
+                  style={{ textDecoration: "none" }}
+                >
+                  <span className="hover:bg-lighter-purple px-3 py-2 rounded-md text-xl font-bold font-archivo cursor-pointer block text-center text-white">
+                    Log Out
+                  </span>
+                </Link>
+              </>
+            ) : (
+              <Link href="/login" style={{ textDecoration: "none" }}>
+                <span className="hover:bg-lighter-purple px-3 py-2 rounded-md text-xl font-archivo cursor-pointer block text-center text-white">
+                  Log in
+                </span>
+              </Link>
+            )}
+          </div>
+        </div>
+      </div>
+    </nav>
   );
 }
