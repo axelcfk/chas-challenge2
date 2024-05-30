173494e166240eacbee707fa8cd4a7fa3e17c7b0
maek95 <mekstrom01@gmail.com>
2024-05-30 13:20:34 +0200

    skeleton loaders Popularmovies,mixpage,watchlist

--- frontend/src/app/components/MovieCardWatchAndLike.jsx
+++ frontend/src/app/components/MovieCardWatchAndLike.jsx
@@ -40,6 +40,7 @@ export default function MovieCardWatchAndLike({
     return <div>Loading...</div>;
   }
 
+
   const serviceLogos = {
     Netflix: "/Netflix1.svg",
     "HBO Max": "/HBO1.svg",
@@ -88,7 +89,7 @@ export default function MovieCardWatchAndLike({
                   postRemoveFromLikeList(id, "movie", title);
                 }
               }}
-              className="box-border absolute top-0 right-0 rounded-tr-2xl rounded-bl-md h-16 w-12 flex justify-center items-center hover:cursor-pointer"
+              className="box-border absolute top-0 left-0 rounded-tl-2xl //rounded-tr-2xl rounded-bl-md h-16 w-12 flex justify-center items-center hover:cursor-pointer"
             >
               {!liked ? (
                 <div className="flex flex-col justify-center items-center">
