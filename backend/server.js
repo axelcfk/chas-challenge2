3a548609bc2f56ab797bc088b8aaa2cc6d2814aa
axelcfk <131769961+axelcfk@users.noreply.github.com>
2024-06-12 08:19:05 +0200

    .

--- backend/server.js
+++ backend/server.js
@@ -23,20 +23,42 @@ const host = "http://16.171.5.238:3010";
 
 app.use(cookieParser());
 
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+// // CORS-konfiguration
+// const corsOptions = {
+//   origin: [
+//     "http://16.171.5.238:3000",
+//     "https://ludi-app.com",
+//     "https://www.ludi-app.com",
+//     "http://ludi-app.com:3000",
+//   ],
+//   credentials: true,
+//   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+//   allowedHeaders: ["Content-Type", "Authorization"],
+// };
+
+// app.options("*", cors(corsOptions)); // Handle preflight requests
+
+// app.use(cors(corsOptions));
+// app.use(bodyParser.json());
+
+// // Middleware for setting headers (applied globally)
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", req.headers.origin);
+//   res.header("Access-Control-Allow-Credentials", "true");
+//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+//   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+//   next();
+// });
+
 // CORS-konfiguration
 const corsOptions = {
-  origin: [
-    "http://16.171.5.238:3000",
-    "https://ludi-app.com",
-    "https://www.ludi-app.com",
-    "http://ludi-app.com:3000",
-  ],
+  origin: ["http://16.171.5.238:3000", "https://ludi-app.com:3000"],
   credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
 };
-
-app.options("*", cors(corsOptions)); // Handle preflight requests
 
 app.use(cors(corsOptions));
 app.use(bodyParser.json());
@@ -45,12 +67,10 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-// Middleware for setting headers (applied globally)
+// Middleware för att sätta CORS-rubriker korrekt
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", req.headers.origin);
+  res.header("Access-Control-Allow-Origin", "http://16.171.5.238:3000");
   res.header("Access-Control-Allow-Credentials", "true");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
 });
 
