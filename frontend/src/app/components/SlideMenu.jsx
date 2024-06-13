0dbadff64f5c37f082273070f7fb687073b7054a
maek95 <mekstrom01@gmail.com>
2024-06-13 11:22:28 +0200

    scroll-button for slidemenu on ipad/desktop

--- frontend/src/app/components/SlideMenu.jsx
+++ frontend/src/app/components/SlideMenu.jsx
@@ -1,28 +1,70 @@
+"use client"
 import Link from "next/link";
 import PropTypes from "prop-types";
-import { FaArrowRight } from "react-icons/fa";
+import { useEffect, useRef, useState } from "react";
+import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
 
 export default function SlideMenu({ children, placeholder = false }) {
+  const scrollContainerRef = useRef(null);
+  const [isRightClicked, setIsRightClicked] = useState(false);
+  const [scrollLeft, setScrollLeft] = useState(0);
+
+  const handleScrollRight = () => {
+    if (scrollContainerRef.current) {
+      const scrollAmount = scrollContainerRef.current.clientWidth * 1; // 100% of the container's width
+      scrollContainerRef.current.scrollBy({
+        left: scrollAmount,
+        behavior: 'smooth'
+      });
+      setIsRightClicked(true);
+      setScrollLeft(scrollContainerRef.current.scrollLeft + scrollAmount);
+    }
+  };
+
+  const handleScrollLeft = () => {
+    if (scrollContainerRef.current) {
+      const scrollAmount = scrollContainerRef.current.clientWidth * 1; // 100% of the container's width
+      scrollContainerRef.current.scrollBy({
+        left: -scrollAmount, // scroll left
+        behavior: 'smooth'
+      });
+      setScrollLeft(scrollContainerRef.current.scrollLeft - scrollAmount);
+    }
+  };
+
+  useEffect(() => {
+    if (scrollContainerRef.current) {
+      const handleScroll = () => {
+        setScrollLeft(scrollContainerRef.current.scrollLeft);
+      };
+
+      const container = scrollContainerRef.current;
+      container.addEventListener('scroll', handleScroll);
+
+      return () => {
+        container.removeEventListener('scroll', handleScroll);
+      };
+    }
+  }, []);
+
   return (
-    <div className="flex max-w-full ">
+    <div className="flex max-w-full relative">
       {!placeholder ? (
-        <div className="w-full h-full overflow-x-scroll scroll whitespace-nowrap scroll-smooth removeScrollbar ">
-          <style>
-            {`
-        @media (min-width: 459px) {
-          .removeScrollbar::-webkit-scrollbar {
-            display: none; /* Safari and Chrome */
-          }
-          .removeScrollbar {
-            scrollbar-width: none; /* Firefox */
-            -ms-overflow-style: none; /* Internet Explorer 10+ */
-          }
-        }
-      
-      `}
-          </style>
-          {children}
-        </div>
+        <>
+          <div className="relative w-full">
+            <div ref={scrollContainerRef} className="w-full h-full overflow-x-scroll scroll whitespace-nowrap scroll-smooth removeScrollbar">
+              {children}
+            </div>
+            {scrollLeft > 0 && (
+              <button className="hidden md:flex scroll-button-left" onClick={handleScrollLeft}>
+                <FaArrowLeft size={"60px"}></FaArrowLeft>
+              </button>
+            )}
+            <button className="hidden md:flex scroll-button" onClick={handleScrollRight}>
+              <FaArrowRight size={"60px"}></FaArrowRight>
+            </button>
+          </div>
+        </>
       ) : (
         <div className="w-full h-full md:h-80 rounded-2xl relative bg-[#1D1631] card-shadow mx-4">
           <div className="h-80"></div>
@@ -34,7 +76,7 @@ export default function SlideMenu({ children, placeholder = false }) {
           /> */}
           <div className="w-full text-center absolute inset-0 flex items-center justify-center">
             <div className="flex flex-col gap-8 justify-start items-start w-[80%] text-start">
-              <h2 className="flex justify-center items-center  text-3xl  uppercase">
+              <h2 className="flex justify-center items-center text-3xl uppercase">
                 <span className="font-archivo font-extrabold">
                   Start adding to your Watchlist
                 </span>
@@ -48,6 +90,59 @@ export default function SlideMenu({ children, placeholder = false }) {
           </div>
         </div>
       )}
+      <style jsx>{`
+        @media (min-width: 459px) {
+          .removeScrollbar::-webkit-scrollbar {
+            display: none; /* Safari and Chrome */
+          }
+          .removeScrollbar {
+            scrollbar-width: none; /* Firefox */
+            -ms-overflow-style: none; /* Internet Explorer 10+ */
+          }
+        }
+        
+        .scroll-button {
+          position: absolute;
+          top: 50%;
+          right: 5px;
+          transform: translateY(-50%);
+          background-color: rgba(0, 0, 0, 0.5);
+          color: white;
+          border: none;
+          border-radius: 50%;
+          width: 80px;
+          height: 80px;
+         /*  display: flex; */
+          align-items: center;
+          justify-content: center;
+          cursor: pointer;
+        }
+  
+        .scroll-button:hover {
+          background-color: rgba(0, 0, 0, 0.7);
+        }
+
+        .scroll-button-left {
+          position: absolute;
+          top: 50%;
+          left: 5px;
+          transform: translateY(-50%);
+          background-color: rgba(0, 0, 0, 0.5);
+          color: white;
+          border: none;
+          border-radius: 50%;
+          width: 80px;
+          height: 80px;
+          /* display: flex; */
+          align-items: center;
+          justify-content: center;
+          cursor: pointer;
+        }
+  
+        .scroll-button-left:hover {
+          background-color: rgba(0, 0, 0, 0.7);
+        }
+      `}</style>
     </div>
   );
 }
